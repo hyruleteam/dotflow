eae75264609d4bcd9cf927cd387ab747307d51ac
王文宇 <wangwenyu9394@gmail.com>
2018-05-25 23:29:58 +0800

    update:更新更多命令

--- src/renderer/components/consoleWin/index.js
+++ src/renderer/components/consoleWin/index.js
@@ -15,12 +15,6 @@ const menuCnt = (
     <Menu.Item>
       <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">1st menu item</a>
     </Menu.Item>
-    <Menu.Item>
-      <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">2nd menu item</a>
-    </Menu.Item>
-    <Menu.Item>
-      <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">3rd menu item</a>
-    </Menu.Item>
   </Menu>
 )
 
